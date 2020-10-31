914569947e5306f96229135c04f63eddf1f00274
Márcio Koch <lobokoch@gmail.com>
2020-10-31 15:21:08 -0300

    Detecção de faces usando OpenCV.

---
+++ face-detection/src/br/com/furb/FaceDetection.java
@@ -0,0 +1,47 @@
+package br.com.furb;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+public class FaceDetection {
+
+	public static void main(String[] args) {
+		
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		
+		final String cascadeFile = "D:\\mk\\OpenCV440\\install\\etc\\haarcascades\\haarcascade_frontalface_alt.xml";
+		CascadeClassifier classifier = new CascadeClassifier(cascadeFile);
+		
+		String filename = "D:\\dataset\\orl\\avengers.jpg";
+		Mat detected = detectFaces(filename, classifier);
+		Imgcodecs.imwrite("D:\\dataset\\orl\\avengers_detected.jpg", detected);
+		
+		System.out.println("DONE!");
+	}
+
+	private static Mat detectFaces(String filename, CascadeClassifier classifier) {
+		Mat img = Imgcodecs.imread(filename);
+		
+		Mat grayImg = new Mat();
+		Imgproc.cvtColor(img, grayImg, Imgproc.COLOR_BGR2GRAY);
+		
+		Imgproc.equalizeHist(grayImg, grayImg);
+		
+		MatOfRect objects = new MatOfRect();
+		classifier.detectMultiScale(grayImg, objects, 1.1, 1);
+		
+		objects.toList().forEach(rect -> {
+			Scalar red = new Scalar(0, 0, 255);
+			//Rect faceRect = new Rect(rect.tl(), rect.br());
+			Imgproc.rectangle(img, rect/*faceRect*/, red, 3);
+		});
+		
+		return img;
+	}
+
+}
